83174fc22e77959cf59a295f64debc3074719624
renandarochadadalto-lgtm <renandarochadadalto@gmail.com>
2025-10-08 11:51:49 -0300

    Update Program.cs

--- Resolução de problemas/Resolução de problemas/Program.cs
+++ Resolução de problemas/Resolução de problemas/Program.cs
@@ -4,10 +4,8 @@ class Program
 {
     static void Main()
     {
-        // Definir os 6 números
         int[] numeros = new int[6];
 
-        // Preencher o array com os 6 números
         Console.WriteLine("Digite 6 números:");
 
         for (int i = 0; i < 6; i++)
@@ -16,24 +14,20 @@ class Program
             numeros[i] = int.Parse(Console.ReadLine());
         }
 
-        // Inicializar o menor e maior número com o primeiro número do array
         int menor = numeros[0];
         int maior = numeros[0];
 
-        // Percorrer o array para encontrar o menor e maior número
         for (int i = 1; i < numeros.Length; i++)
         {
             if (numeros[i] < menor)
             {
-                menor = numeros[i]; // Atualiza o menor
+                menor = numeros[i];
             }
             if (numeros[i] > maior)
             {
-                maior = numeros[i]; // Atualiza o maior
+                maior = numeros[i];
             }
         }
-
-        // Exibir o resultado
         Console.WriteLine($"\nO menor número é: {menor}");
         Console.WriteLine($"O maior número é: {maior}");
     }
